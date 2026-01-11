bcc1f8bcc750dfc02019b4c9f78293d44339b522
devdiagon <fredericktimo@gmail.com>
2026-01-11 16:44:47 -0500

    feat: add title description

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -3,8 +3,12 @@ import { AnimText } from "../components";
 export const Home = () => {
 
   return (
-    <>
+    <div className="flex flex-col max-w-md p-2 gap-4">
       <AnimText />
-    </>
+      <p className="text-md">
+        Software developer who enjoys creating useful solutions for the community. I love creating user interfaces for
+        <span className="bg-gradient-to-br from-red-500 to-orange-500 bg-clip-text text-transparent"> web and mobile apps.</span>
+      </p>
+    </div>
   );
 }

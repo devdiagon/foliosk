0e572e170aa06fc5aef2b6d8c5c0089d189a53d7
devdiagon <fredericktimo@gmail.com>
2026-01-11 18:38:55 -0500

    feat: add milestones data

---
+++ src/content/milestones.ts
@@ -0,0 +1,33 @@
+export interface Milestone {
+  title: string;
+  description: string;
+  location: string;
+  period: string;
+}
+
+export const education: Milestone[] = [
+  {
+    title: "Universidad de las Fuerzas Armadas ESPE",
+    description: "Software Engineering student",
+    location: "Sangolquí, Ecuador",
+    period: "2022 - Present",
+  },
+  {
+    title: "Egg",
+    description: "Online programming course for beginners certificate",
+    location: "Quito, Ecuador",
+    period: "2022",
+  },
+  {
+    title: "International Schoolastic Model (ISM)",
+    description: "Bachillerato General Unificado (BGU)",
+    location: "Quito, Ecuador",
+    period: "2022",
+  },
+  {
+    title: "Cambridge Assessment English",
+    description: "B2 C grade certificate",
+    location: "Quito, Ecuador",
+    period: "2021",
+  },
+];

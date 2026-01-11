9ac31c1f1054053139d6b2d489a2a35ae8fc311a
devdiagon <fredericktimo@gmail.com>
2026-01-11 17:55:28 -0500

    style: initial style for sections

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -1,3 +1,19 @@
+import { MdOutlineMenuBook } from "react-icons/md";
+
 export const Education = () => {
-  return <h2>Education</h2>
+  return (
+      <div className="max-w-5xl mx-auto px-6 py-8 space-y-8">
+        <header className="flex items-center gap-3">
+          <MdOutlineMenuBook size={36} />
+          <h2 className="text-2xl font-light text-black">
+            Education
+          </h2>
+        </header>
+  
+        {/* Contenido */}
+        <div className="w-full min-h-[120px] bg-white/60 backdrop-blur-md rounded-2xl p-6 shadow-sm">
+          
+        </div>
+      </div>
+    );
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,3 +1,19 @@
+import { FaCubes } from "react-icons/fa6";
+
 export const Projects = () => {
-  return <h2>Projects</h2>
+  return (
+    <div className="max-w-5xl mx-auto px-6 py-8 space-y-8">
+      <header className="flex items-center gap-3">
+        <FaCubes size={36} />
+        <h2 className="text-2xl font-light text-black">
+          Projects
+        </h2>
+      </header>
+
+      {/* Contenido */}
+      <div className="w-full min-h-[120px] bg-white/60 backdrop-blur-md rounded-2xl p-6 shadow-sm">
+        
+      </div>
+    </div>
+  );
 }
